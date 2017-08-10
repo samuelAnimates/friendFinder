4765bfd3071f22ad1817abea9d4621afa95c4cac
smendez92 <samuelremndez14@gmail.com>
2017-08-09 23:25:22 -0500

    HTML routes set up and POST route in place

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,7 +14,10 @@ module.exports = function(app) {
 	// API GET requests
 	// =============================================================
 
-	//A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+	/*
+	A GET route with the url `/api/friends`. This will be used to display
+	a JSON of all possible friends.
+	*/
 	app.get("/api/friends", function(request, response) {
 		response.json(friendsData);
 	});
@@ -23,7 +26,15 @@ module.exports = function(app) {
 	// API POST requests
 	// =============================================================
 
-	// A POST routes `/api/friends`. This will be used to handle incoming survey results.
-	// This route will also be used to handle the compatibility logic.
+	/*
+	A  POST routes `/api/friends`. This will be used to handle incoming survey results
+	and handle the compatibility logic.
+	*/
+	
+	app.post("/api/tables", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    	console.log("POST");
+	});
 
 };
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // ================================================================================
 
 require("./app/routing/apiRoutes.js")(app);
-//require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 
 // Starts the server to begin listening
