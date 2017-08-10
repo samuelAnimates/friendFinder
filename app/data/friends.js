33db0ff07c09bc8f766c934fdbb7bf2de1b0699c
smendez92 <samuelremndez14@gmail.com>
2017-08-09 21:59:10 -0500

    friends objects defined initially

---
+++ app/data/friends.js
@@ -0,0 +1,77 @@
+// Friends (DATA)
+[{
+  "name": "Doug",
+  "photoURL": "../public/assets/images/Doug.jpg",
+  "scores":[
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ]
+}, {
+  "name": "Kat",
+  "photoURL": "../public/assets/images/Kat.jpg",
+  "scores":[
+    1,
+    2,
+    3,
+    4,
+    5,
+    1,
+    2,
+    3,
+    4,
+    5
+  ]
+}, {
+  "name": "Lizzy",
+  "photoURL": "../public/assets/images/Lizzy.jpg",
+  "scores":[
+    3,
+    4,
+    2,
+    2,
+    3,
+    4,
+    5,
+    3,
+    2,
+    4
+  ]
+}, {
+  "name": "Mary",
+  "photoURL": "../public/assets/images/Mary.jpg",
+  "scores":[
+    2,
+    3,
+    1,
+    1,
+    2,
+    3,
+    4,
+    2,
+    1,
+    3
+  ]
+}, {
+  "name": "Cam",
+  "photoURL": "../public/assets/images/Cam.jpg",
+  "scores":[
+    1,
+    4,
+    2,
+    2,
+    1,
+    5,
+    4,
+    1,
+    2,
+    5
+  ]
+}];
---
+++ server.js
@@ -0,0 +1,21 @@
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//access environmental variables for username, password, host
+require('dotenv').config({path: './envVar.env'});
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 5000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
