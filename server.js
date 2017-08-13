1ba0cf81993293bf6ae16d393e112fce6c5806df
smendez92 <samuelremndez14@gmail.com>
2017-08-13 00:33:54 -0500

    server fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('dotenv').config({path: './dotenv.env'});
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
